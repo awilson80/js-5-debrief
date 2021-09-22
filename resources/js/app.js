31fac4c2fb26cdeb759011c149826c65c4f55c65
unknown <AlyssaNWilson@gmail.com>
2021-09-22 15:17:30 -0500

    Adds code according to the instructions (no stretch goals)

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,37 @@
 // 5. A button named 'Clear' that removes the populated list from the screen
 // 6. The application should be designed to be visually pleasing. 
 // Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-5).
+
+const names = ['Alyssa', 'Raven', 'Yannick', 'Anthony', 'Martin', 'Alexandra', 'Hector', 'Perom', 'Kaleab', 'Willis'];
+
+const getNameButton = document.getElementById('randomNamesButton');
+const clearButton = document.getElementById('clearButton');
+const namesList = document.getElementById('namesList');
+
+// Function creating list items as 4 names from names array populate list div
+
+const clearBox = () => {
+  namesList.innerHTML = '';
+};
+
+let generateRandomName = () => {
+    clearBox();
+    let i = 0;
+    while (i < 4) {
+
+        let randomName = names[Math.floor(Math.random()*names.length)];
+
+        let listItem = document.createElement("li");
+        listItem.id = 'nameListItem'
+        listItem.innerHTML = randomName // Result of the while loop - delivers a random name
+        namesList.appendChild(listItem);
+
+      i++;
+
+    }
+};
+
+getNameButton.onclick = generateRandomName;
+
+clearButton.onclick = clearBox;
+
